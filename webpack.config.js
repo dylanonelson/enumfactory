3bbd202578e9ea864b15e46f34ef6cc5f1559bce
dylanonelson <dylanonelson@gmail.com>
2017-02-03 21:22:03 -0500

    Initialize repository

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    contentBase: 'dist',
+    inline: true,
+    port: 1104
+  },
+  devtool: 'source-map',
+  entry: './src/index.js',
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: ['babel-loader'],
+    },{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
+    }, {
+      enforce: 'pre',
+      include: /src/,
+      use: [{
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      }],
+      test: /\.js?$/,
+    }],
+  },
+  output: {
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'Enum',
+    path: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+  }
+}
